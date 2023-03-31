22bcec25de4264ada5994a4caa8e73cda673d6b1
queenaanu <queenaanu@gmail.com>
2023-03-31 01:31:18 +0100

    First commit  Changes to be committed: 	new file:   .gitignore 	new file:   Gallery/gallery.js 	new file:   Gallery/index.html 	new file:   Gallery/jpeg/cod3.jpg 	new file:   Gallery/jpeg/pic0.jpg 	new file:   Gallery/jpeg/pic1.jpg 	new file:   Gallery/jpeg/pic10.jpg 	new file:   Gallery/jpeg/pic11.jpg 	new file:   Gallery/jpeg/pic2.jpg 	new file:   Gallery/jpeg/pic3.jpg 	new file:   Gallery/jpeg/pic4.jpg 	new file:   Gallery/jpeg/pic5.jpg 	new file:   Gallery/jpeg/pic6.jpg 	new file:   Gallery/jpeg/pic7.jpg 	new file:   Gallery/jpeg/pic8.jpg 	new file:   Gallery/jpeg/pic9.jpg 	new file:   Gallery/output.css 	new file:   input.css 	new file:   package-lock.json 	new file:   package.json 	new file:   tailwind.config.js

---
+++ Gallery/gallery.js
@@ -0,0 +1,71 @@
+const change = document.getElementById("image1");
+change.addEventListener("mousedown", (event) => {
+  event.target.style.background = "red";
+  event.target.style.color = "white";
+});
+
+const change2 = document.getElementById("image2");
+change2.addEventListener("mouseover", (event) => {
+event.target.style.background = "purple";
+event.target.style.color = "white";
+});
+
+const change3 = document.getElementById("image3");
+change3.addEventListener("mouseout", (event) => {
+event.target.style.background = "pink";
+event.target.style.color = "white";
+});
+
+const change4 = document.getElementById("image4");
+change4.addEventListener("click", (event) => {
+event.target.style.background = "orange";
+event.target.style.color = "white";
+});
+
+const change5 = document.getElementById("image5");
+change5.addEventListener("keypress", (event) => {
+  event.target.style.background = "red";
+  event.target.style.color = "white";
+});
+
+const change6 = document.getElementById("image6");
+change6.addEventListener("keydown", (event) => {
+event.target.style.background = "brown";
+event.target.style.color = "white";
+});
+
+const change7 = document.getElementById("image7");
+change7.addEventListener("click", (event) => {
+event.target.style.background = "brown";
+event.target.style.color = "white";
+});
+
+const change8 = document.getElementById("image8");
+change8.addEventListener("mouseover", (event) => {
+event.target.style.background = "green";
+event.target.style.color = "white";
+});
+
+const change9 = document.getElementById("image9");
+change9.addEventListener("keydown", (event) => {
+event.target.style.background = "grey";
+event.target.style.color = "white";
+});
+
+const change10 = document.getElementById("image10");
+change10.addEventListener("mouseup", (event) => {
+event.target.style.background = "black";
+event.target.style.color = "white";
+});
+
+const change11 = document.getElementById("image11");
+change11.addEventListener("keyup", (event) => {
+event.target.style.background = "red";
+event.target.style.color = "white";
+});
+
+const change12 = document.getElementById("image12");
+change12.addEventListener("click", (event) => {
+event.target.style.background = "pink";
+event.target.style.color = "white";
+});
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./Gallery/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md : '768px',
+      lg:  '976px',
+      xl:  '1440px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
